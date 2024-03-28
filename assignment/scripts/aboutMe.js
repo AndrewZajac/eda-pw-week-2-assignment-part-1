8c116c6813e5ae5267994d9bc8112115cd0764e4
AndrewZajac <andrewzajac05@gmail.com>
2024-03-28 10:42:07 -0500

    #14 pretty much done, need to figure out the const of allowedPets

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -14,7 +14,7 @@ let food = 'pasta';
 let pets = 9;
 let friendsPets = 4;
 const petsAllowed = 12;
-let result = true;
+let result;
 
 console.log('My full name is:', fullName);
 
@@ -77,9 +77,15 @@ console.log(diceRoll)
 //      set `petStatus` to the value of "I have enough pets" 
 //      if the value of `pets` is greater than the value of `allowedPets`.
 //      set `petStatus` to the value of "Oh no, I have too many pets!"
-let petStatus = true;
+let petStatus;
 if(pets << allowedPets){
   petStatus = "I can have more pets";
+}
+else if(pets == allowedPets){
+petStatus = "I have enough pets";
+}
+else if(pets >> allowedPets){
+  petStatus = "Oh no, I have too many pets!";
 }
 console.log(petStatus);
 
