3c4ca604dd8036e434a16c5638bb7e20f0c465b0
AndrewZajac <andrewzajac05@gmail.com>
2024-03-30 19:49:49 -0500

    fixed error of spelling with question 11, also changed title in html to Hello Minsky

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -13,7 +13,7 @@ let adventurous = true;
 let food = 'pasta';
 let pets = 9;
 let friendsPets = 4;
-const petsAllowed = 12;
+const allowedPets = 12;
 let result;
 
 console.log('My full name is:', fullName);
@@ -78,14 +78,14 @@ console.log(diceRoll)
 //      if the value of `pets` is greater than the value of `allowedPets`.
 //      set `petStatus` to the value of "Oh no, I have too many pets!"
 let petStatus;
-if(pets << allowedPets){
-  petStatus = "I can have more pets";
+if(pets < allowedPets){
+  petStatus = 'I can have more pets';
 }
-else if(pets == allowedPets){
-petStatus = "I have enough pets";
+else if(pets === allowedPets){
+petStatus = 'I have enough pets';
 }
-else if(pets >> allowedPets){
-  petStatus = "Oh no, I have too many pets!";
+else if(pets > allowedPets){
+  petStatus = 'Oh no, I have too many pets!';
 }
 console.log(petStatus);
 
@@ -97,6 +97,8 @@ console.log(petStatus);
 //      assigns the highest value to `mostPets`. There's several possibilities --
 //      be sure to think through all the scenarios. 
 //      console.log `mostPets` after the conditional has run.
+
+
 
 // 16 - Make a variable called `luckyResult`
 //      Write a *switch* statement that sets `luckyResult` to:
